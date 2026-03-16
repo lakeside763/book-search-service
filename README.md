e3d26fda0fead21f80b9ceadfbbc70f1166c99a7
lakeside763 <lakeside763@gmail.com>
2026-03-16 07:30:17 +0000

    chore: project setup

---
+++ README.md
@@ -0,0 +1,7 @@
+core
+  /book.model.ts
+  /book-search.searvice.ts
+  /book-provider.ts
+  /errors.ts
+  /query-validator.ts
+  /http-client.ts
---
+++ src/core/book.model.ts
@@ -0,0 +1,9 @@
+export type Book = {
+  id: string;
+  title: string;
+  author: string[];
+  publisher: string;
+  yearPublished: number;
+  isbn: string;
+  source: string;
+}
